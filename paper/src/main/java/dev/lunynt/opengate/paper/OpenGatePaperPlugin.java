798883d03bdd9483c2ea50fe321207cc281e4190
lunynt <lunynt@proton.me>
2026-09-01 19:08:47 +0300

    add paper support

---
+++ paper/src/main/java/dev/lunynt/opengate/paper/OpenGatePaperPlugin.java
@@ -0,0 +1,36 @@
+package dev.lunynt.opengate.paper;
+
+import dev.lunynt.opengate.OpenGate;
+import java.io.IOException;
+import java.nio.file.Files;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OpenGatePaperPlugin extends JavaPlugin {
+    private OpenGate openGate;
+
+    @Override
+    public void onEnable() {
+        try {
+            Files.createDirectories(getDataFolder().toPath());
+        } catch (IOException exception) {
+            throw new IllegalStateException("could not create OpenGate data directory", exception);
+        }
+        openGate = OpenGate.create(getDataFolder().toPath());
+        getServer().getPluginManager().registerEvents(new PaperAuthenticationListener(this), this);
+        var commands = new PaperAuthenticationCommand(this);
+        java.util.Objects.requireNonNull(getCommand("login")).setExecutor(commands);
+        java.util.Objects.requireNonNull(getCommand("register")).setExecutor(commands);
+        getLogger().info("OpenGate authentication engine enabled on Paper");
+    }
+
+    @Override
+    public void onDisable() {
+        if (openGate != null) {
+            openGate.close();
+        }
+    }
+
+    public OpenGate openGate() {
+        return openGate;
+    }
+}

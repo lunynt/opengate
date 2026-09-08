9fb691bb0f96a1c98b0de06270620bf5d96ad256
Lunynt <lunynt@proton.me>
2026-09-08 16:50:18 +0300

    build: update dependencies and CI actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ subprojects {
     }
 
     dependencies {
-        "testImplementation"(platform("org.junit:junit-bom:5.13.4"))
+        "testImplementation"(platform("org.junit:junit-bom:6.1.3"))
         "testImplementation"("org.junit.jupiter:junit-jupiter")
         "testRuntimeOnly"("org.junit.platform:junit-platform-launcher")
     }
--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
 
 dependencies {
     implementation("org.bouncycastle:bcprov-jdk18on:1.84")
-    implementation("com.zaxxer:HikariCP:7.0.2")
-    implementation("org.xerial:sqlite-jdbc:3.53.1.0")
+    implementation("com.zaxxer:HikariCP:7.1.0")
+    implementation("org.xerial:sqlite-jdbc:3.53.4.0")
     implementation("org.postgresql:postgresql:42.7.8")
     implementation("com.mysql:mysql-connector-j:9.5.0")
     implementation("org.mariadb.jdbc:mariadb-java-client:3.5.6")
     implementation("com.h2database:h2:2.4.240")
-    implementation("io.lettuce:lettuce-core:6.7.1.RELEASE")
+    implementation("io.lettuce:lettuce-core:7.7.0.RELEASE")
     implementation("com.fasterxml.jackson.core:jackson-core:2.22.2")
     compileOnly("org.geysermc.floodgate:api:2.2.5-SNAPSHOT")
     compileOnly("us.ajg0702.queue.api:api:2.9.1")
